a8d7a5a13e7704b55462737caefb22db1ad8ccaa
S2509679 <s2509679@fwn-bborg-edu-74-110.fmnsedu.rug.nl>
2014-09-15 14:15:47 +0200

    expmod && oddPspTO Improved

--- opdr1.hs
+++ opdr1.hs
@@ -9,3 +9,12 @@ cntPrime n = sum[fromEnum (isPrime x) | x <- [1..n]]
 
 oddPspTO :: Integer -> Integer -> [Integer]
 oddPspTO a upb = [n | n <- [3,5..upb], a^(n-1) `mod` n == 1, not (isPrime n)]
+
+expmod :: Integer -> Integer -> Integer -> Integer
+expmod a e n 
+	| e == 1 = a `mod` n
+	| e `mod` 2 == 1 = (a * (expmod (a*a) (e`div`2) n)) `mod` n
+	| e `mod` 2 == 0 = (expmod (a*a) (e`div`2) n) `mod` n
+
+oddPspTOI :: Integer -> Integer -> [Integer]
+oddPspTOI a upb = [n | n <- [3,5..upb], expmod a (n-1) n == 1, not (isPrime n)]
