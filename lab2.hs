2b0189fb0170b3e72c5ea6a002680a2dfa1df5d1
S1959492 <s1959492@fwn-bborg-edu-74-57.fmnsedu.rug.nl>
2014-10-02 16:47:40 +0200

    multiplier done start palin

--- lab2.hs
+++ lab2.hs
@@ -7,9 +7,11 @@ import Data.List
 
 --smallestMultiplier :: (a -> b -> b) -> b -> [a] -> [[a]]
 
--- foldr `div` [n, 2*n ..]
 
-smallestMultiplier n =  foldr (*) 1 (foldr merge [] (map factor [2..n]))
+
+smallestMultiplier n =  foldr lcm 1 [2..n]
+
+-- smallestMultiplier n =  foldr (*) 1 (foldr merge [] (map factor [2..n]))
 
 -- Gives the prime factorisation of a given number.
 factor :: Integer -> [Integer]
@@ -28,3 +30,41 @@ merge (x:xs) (y:ys)
   | x < y     = x : (merge xs (y:ys))
   | x == y    = x : (merge xs ys)
   | otherwise = y : (merge (x:xs) ys)
+  
+-- Merges a list of ordered lists to one ordered list.
+merge2 :: [[Integer]] -> [Integer]
+merge2 [] = []
+merge2(x:xs) = merge x (merge2 xs)  
+
+-- We call a positive integer n a palindromic number if reversing its digits yields the same number. For
+-- example, 123321 is a palindromic number, while the number 123 is clearly not palindromic. Write a
+-- Haskell function numberOfPalindromicComposites that takes as its input a positive integer
+-- n, and produces as its output the number of palindromic numbers x which have the form
+-- x=p*q<n, where p and q are prime numbers. For example, numberOfPalindromicComposites 100
+-- should yield 7 (which is length [4,6,9,22,33,55,77].
+-- The time to compute palindromicComposites (10^8) using ghci should not exceed two seconds.
+
+isPalindromicNumber :: Integer -> Bool
+isPalindromicNumber n = n == read(reverse(show n))
+
+palindromes :: [Integer]
+palindromes = genPalin []
+	where genPalin xs =
+		[(x:xs)| x<-[0..9]]
+		x:(reverse xs)
+	
+-- Sieve of Eratosthenes, is a infinite list.
+primes :: [Integer]
+primes = 2:(sieve [ 3, 5..])
+  where
+  sieve (p:x) = p : sieve [ n | n <- x, n `mod` p > 0 ] 
+  
+-- Helpfunction which gives the square of a integer.
+isqrt :: Integer -> Integer
+isqrt = floor . sqrt . fromIntegral
+
+select :: Integer -> Integer -> [Integer] -> [Integer]
+select m n xs = takeWhile (<= n) (dropWhile (<m) xs) 
+
+numberOfPalindromicComposites n = length[x*y| x<-xs, y<-select x (n `div` x) primes, isPalindromicNumber(x*y)]
+	where xs = select 2 (isqrt n) primes 
