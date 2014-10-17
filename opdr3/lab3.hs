a3ef560c90563fcb55eeb314607d33b99eec7057
S2509679 <s2509679@fwn-bborg-edu-74-89.fmnsedu.rug.nl>
2014-10-17 14:58:33 +0200

    worked on the parser

--- opdr3/lab3.hs
+++ opdr3/lab3.hs
@@ -32,24 +32,17 @@ vars (p :%: q) = nub (vars p ++ vars q)
 -- Part 01 Exercise 04:
 -- Evaluating Exprs
 --evalExpr :: Expr -> Valuation -> Integer
-evalExpr e xs = read (show (hoi e xs))::Integer
+evalExpr e xs = read (show (eval e xs))::Integer
 
-hoi (Var x) xs= head [snd n | n <- xs, (fst n) == x] 
-hoi (Val x) xs= x
-hoi (p :+: q) xs= (hoi p xs) + (hoi q xs)
-hoi (p :-: q) xs= (hoi p xs) - (hoi q xs)
-hoi (p :*: q) xs= (hoi p xs) * (hoi q xs)
-hoi (p :/: q) xs= div (hoi p xs) (hoi q xs)
-hoi (p :%: q) xs= mod (hoi p xs) (hoi q xs)
+eval (Var x) xs= head [snd n | n <- xs, (fst n) == x] 
+eval (Val x) xs= x
+eval (p :+: q) xs= (eval p xs) + (eval q xs)
+eval (p :-: q) xs= (eval p xs) - (eval q xs)
+eval (p :*: q) xs= (eval p xs) * (eval q xs)
+eval (p :/: q) xs= div (eval p xs) (eval q xs)
+eval (p :%: q) xs= mod (eval p xs) (eval q xs)
 
-{--valid :: Expr -> Valuation -> [Bool]
-valid (Var x) = head [snd n | n <- xs, (fst n) == x] 
-valid (Val x) = x
-valid (p :+: q) = valid p + valid q
-valid (p :-: q) = valid p - valid q
-valid (p :*: q) = valid p * valid q
-valid (p :/: q) = valid p `div` valid q
-valid (p :%: q) = valid p `mod` valid q--}
+
 
 -- Part 01 Exercise 05:
 -- Parsing expressions: String to Expr
@@ -62,15 +55,54 @@ tokenize (x:xs)
 	| isAlpha x = (x:takeWhile isAlpha xs) : tokenize (dropWhile isAlpha xs)
 	| isDigit x = (x:takeWhile isDigit xs) : tokenize (dropWhile isDigit xs)
 	| otherwise = error "Syntax Error: invalid character in input"
+
+parseE :: [String] -> (Expr,[String])
+parseE tokens = parseE' acc rest
+  where (acc, rest) = parseT tokens
+  
+parseE' :: Expr -> [String] -> (Expr,[String])
+parseE' accepted ("+":tokens) =  
+  let (acc,rest) = parseT (accepted :+: fsttokensPlus) 
+    in parseE' acc rest    
+    where fsttokensPlus = fst (parseE' tokens)   
+parseE' accepted ("-":tokens) = 
+  let (acc,rest) = parseT (accepted :-: fsttokensMin) 
+    in parseE' acc rest 
+    where fsttokensMin = fst (parseE' tokens)   
+parseE' accepted tokens =  (accepted, tokens)
+
+parseT :: [String] -> (Expr,[String])
+parseT tokens = parseT' acc rest
+  where (acc, rest) = parseF tokens
+  
+parseT' :: Expr -> [String] -> (Expr,[String])
+parseT' accepted ("*":tokens) =  
+  let (acc,rest) = parseF (accepted :*:  fsttokensTimes) 
+    in parseT' acc rest  
+    where fsttokensTimes = fst (parseT' tokens)   
+parseT' accepted ("/":tokens) = 
+  let (acc,rest) = parseF (accepted :/: fsttokensDiv) 
+    in parseT' acc rest 
+    where fsttokensDiv = fst (parseT' tokens)   
+parseT' accepted ("%":tokens) = 
+  let (acc,rest) = parseF (accepted :%: fsttokensMod) 
+    in parseT' acc rest 
+    where fsttokensMod = fst (parseT' tokens)   
+parseT' accepted tokens =  (accepted, tokens)
 	
-{--expres :: [[Char]] -> String
-expres [] = []
-expres (x:xs)
-	| isDigit x = (read x::Integer): ")" : expres xs
-	| isAlpha x = (read x::Char): ")" : expres xs
-	| elem x "+/-%" = "(" : (read x::thingie) : expres xs
+	
+parseF :: [String] -> (Expr,[String])
+parseF [] = error "Parse error...abort"
+parseF (tok:tokens)
+	| tok == "("		   = (expr, tail rest)
+	| isAlpha	(head tok) = (Var tok, tokens)
+	| isDigit	(head tok) = (Val (read tok), tokens)
+	| otherwise 		 = error("Syntax Error: " ++ tok)
+	where
+		(expr, rest) = parseE tokens
+	
+parser :: String -> (Expr,[String])
+parser str = parseE (tokenize str)
 
---toExpr :: String -> Expr
-toExpr e = expres (tokenize e)
-
---}
+toExpr :: String -> Expr
+toExpr str = fst (parser str)
