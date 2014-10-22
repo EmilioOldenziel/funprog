661b5e92eb0053fdde704a045fbe7efe1a8ebc11
S1959492 <s1959492@fwn-bborg-edu-74-46.fmnsedu.rug.nl>
2014-10-22 17:07:41 +0200

    started with part 2

--- opdr3/Expression.hs
+++ opdr3/Expression.hs
@@ -1,7 +1,11 @@
 module Expression where
-	type Name = String
-	
-	data Expr =
+import Data.List
+import Data.Function
+import Data.Char
+import Types
+import Valuations
+
+data Expr =
 			Val Integer
 		| Var Name
 		| Expr :+: Expr
@@ -9,3 +13,53 @@ module Expression where
 		| Expr :*: Expr
 		| Expr :/: Expr
 		| Expr :%: Expr
+		
+
+-- Part 01 Exercise 06:
+-- Wrap it all up
+
+parseF :: [String] -> (Expr,[String])
+parseF [] = error "Parse error...abort"
+parseF (tok:tokens)
+	| tok == "("		   = (expr, tail rest)
+	| isAlpha	(head tok) = (Var tok, tokens)
+	| isDigit	(head tok) = (Val (read tok), tokens)
+	| otherwise 		 = error("Syntax Error: " ++ tok)
+	where
+		(expr, rest) = parseE tokens
+		
+parseT' :: Expr -> [String] -> (Expr,[String])
+parseT' accepted ("*":tokens) = parseT' (accepted :*: term) rest
+	where (term, rest) = parseF tokens
+parseT' accepted ("/":tokens) = parseT' (accepted :/: term) rest
+    where (term, rest) = parseF tokens
+parseT' accepted ("%":tokens) = parseT' (accepted :%: term) rest
+    where (term, rest) = parseF tokens
+parseT' accepted tokens =  (accepted, tokens)
+
+parseT :: [String] -> (Expr,[String])
+parseT tokens = parseT' acc rest
+  where (acc, rest) = parseF tokens
+
+parseE' :: Expr -> [String] -> (Expr,[String])
+parseE' accepted ("+":tokens) = parseE' (accepted :+: term) rest
+	where (term, rest) = parseT tokens
+parseE' accepted ("-":tokens) = parseE' (accepted :-: term) rest
+	where (term, rest) = parseT tokens
+parseE' accepted tokens = (accepted, tokens)
+
+parseE :: [String] -> (Expr,[String])
+parseE tokens = parseE' acc rest
+  where (acc, rest) = parseT tokens
+  
+tokenize :: String -> [String]
+tokenize [] = []
+tokenize (x:xs)
+	| elem x "*+/-%()" = [x] : (tokenize xs)	
+	| elem x " " = tokenize xs
+	| isAlpha x = (x:takeWhile isAlpha xs) : tokenize (dropWhile isAlpha xs)
+	| isDigit x = (x:takeWhile isDigit xs) : tokenize (dropWhile isDigit xs)
+	| otherwise = error "Syntax Error: invalid character in input"
+
+parser :: String -> (Expr,[String])
+parser str = parseE (tokenize str)
--- opdr3/lab3.hs
+++ opdr3/lab3.hs
@@ -1,6 +1,8 @@
 import Data.List
 import Data.Function
 import Data.Char
+
+import Compare
 import Expression
 import Types
 import Valuations
@@ -31,9 +33,8 @@ vars (p :%: q) = nub (vars p ++ vars q)
 
 -- Part 01 Exercise 04:
 -- Evaluating Exprs
---evalExpr :: Expr -> Valuation -> Integer
+evalExpr :: Expr -> Valuation -> Integer
 evalExpr e xs = read (show (eval e xs))::Integer
-
 eval (Var x) xs= head [snd n | n <- xs, (fst n) == x] 
 eval (Val x) xs= x
 eval (p :+: q) xs= (eval p xs) + (eval q xs)
@@ -42,67 +43,17 @@ eval (p :*: q) xs= (eval p xs) * (eval q xs)
 eval (p :/: q) xs= div (eval p xs) (eval q xs)
 eval (p :%: q) xs= mod (eval p xs) (eval q xs)
 
-
-
 -- Part 01 Exercise 05:
 -- Parsing expressions: String to Expr
-
---tokenize :: [Char] -> [[Char]]
-tokenize [] = []
-tokenize (x:xs)
-	| elem x "*+/-%()" = [x] : (tokenize xs)	
-	| elem x " " = tokenize xs
-	| isAlpha x = (x:takeWhile isAlpha xs) : tokenize (dropWhile isAlpha xs)
-	| isDigit x = (x:takeWhile isDigit xs) : tokenize (dropWhile isDigit xs)
-	| otherwise = error "Syntax Error: invalid character in input"
-
-parseE :: [String] -> (Expr,[String])
-parseE tokens = parseE' acc rest
-  where (acc, rest) = parseT tokens
-  
-parseE' :: Expr -> [String] -> (Expr,[String])
-parseE' accepted ("+":tokens) =  
-  let (acc,rest) = parseT (accepted :+: fsttokensPlus) 
-    in parseE' acc rest    
-    where fsttokensPlus = fst (parseE' tokens)   
-parseE' accepted ("-":tokens) = 
-  let (acc,rest) = parseT (accepted :-: fsttokensMin) 
-    in parseE' acc rest 
-    where fsttokensMin = fst (parseE' tokens)   
-parseE' accepted tokens =  (accepted, tokens)
-
-parseT :: [String] -> (Expr,[String])
-parseT tokens = parseT' acc rest
-  where (acc, rest) = parseF tokens
-  
-parseT' :: Expr -> [String] -> (Expr,[String])
-parseT' accepted ("*":tokens) =  
-  let (acc,rest) = parseF (accepted :*:  fsttokensTimes) 
-    in parseT' acc rest  
-    where fsttokensTimes = fst (parseT' tokens)   
-parseT' accepted ("/":tokens) = 
-  let (acc,rest) = parseF (accepted :/: fsttokensDiv) 
-    in parseT' acc rest 
-    where fsttokensDiv = fst (parseT' tokens)   
-parseT' accepted ("%":tokens) = 
-  let (acc,rest) = parseF (accepted :%: fsttokensMod) 
-    in parseT' acc rest 
-    where fsttokensMod = fst (parseT' tokens)   
-parseT' accepted tokens =  (accepted, tokens)
-	
-	
-parseF :: [String] -> (Expr,[String])
-parseF [] = error "Parse error...abort"
-parseF (tok:tokens)
-	| tok == "("		   = (expr, tail rest)
-	| isAlpha	(head tok) = (Var tok, tokens)
-	| isDigit	(head tok) = (Val (read tok), tokens)
-	| otherwise 		 = error("Syntax Error: " ++ tok)
-	where
-		(expr, rest) = parseE tokens
-	
-parser :: String -> (Expr,[String])
-parser str = parseE (tokenize str)
-
 toExpr :: String -> Expr
 toExpr str = fst (parser str)
+
+-- Part 01 Exercise 01:
+-- toComparison
+toComparison :: String -> Comparison
+toComparison (p < q) = Cmp LessThan p q
+toComparison (p <= q) = Cmp LessEqual p q
+toComparison (p = q) = Cmp Equal p q
+toComparison (p > q) = Cmp GreaterThan p q
+toComparison (p >= q) = Cmp GreaterThan p q
+toComparison (p # q) = Cmp NotEqual p q
