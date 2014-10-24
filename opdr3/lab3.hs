d201e3f8ae8abc7c393c6a42e62fa1dbe9492057
S1959492 <s1959492@fwn-bborg-edu-74-47.fmnsedu.rug.nl>
2014-10-24 16:21:16 +0200

    added types

--- opdr3/lab3.hs
+++ opdr3/lab3.hs
@@ -15,13 +15,17 @@ import Valuations
 toComparison :: String -> Comparison
 toComparison str = Cmp (checkOperator (getCom str)) (toExpr(eOne str)) (toExpr(eTwo str))
 
+eOne :: [Char] -> [Char]
 eOne str = takeWhile (not.(\x -> elem x "<>=#")) str
 
+eTwo ::[Char] -> [Char]
 eTwo str = reverse (takeWhile (not.(\x -> elem x "<>=#")) (reverse str))
 
 --takes the comparator sign from a expression string
+getCom:: [Char] -> [Char]
 getCom str = (reverse (dropWhile (not.(\x -> elem x "<>=#")) (reverse (dropWhile (not.(\x -> elem x "<>=#")) str))))
 
+checkOperator:: [Char] -> Relop
 checkOperator "<" = LessThan
 checkOperator "<=" = LessEqual
 checkOperator "=" = Equal
@@ -31,10 +35,10 @@ checkOperator "#" = NotEqual
 
 
 evalCmp :: Comparison -> Valuation -> Bool
-evalCmp (Cmp LessThan eOne eTwo) xs 		= (evalExpr eOne xs) < 	(evalExpr eTwo xs)
-evalCmp (Cmp LessEqual eOne eTwo) xs 		= (evalExpr eOne xs) <= (evalExpr eTwo xs)
-evalCmp (Cmp Equal eOne eTwo) xs 			= (evalExpr eOne xs) == (evalExpr eTwo xs)
+evalCmp (Cmp LessThan eOne eTwo) xs 	= (evalExpr eOne xs) < 	(evalExpr eTwo xs)
+evalCmp (Cmp LessEqual eOne eTwo) xs 	= (evalExpr eOne xs) <= (evalExpr eTwo xs)
+evalCmp (Cmp Equal eOne eTwo) xs 		= (evalExpr eOne xs) == (evalExpr eTwo xs)
 evalCmp (Cmp Greater eOne eTwo) xs 		= (evalExpr eOne xs) > 	(evalExpr eTwo xs)
-evalCmp (Cmp GreaterEqual eOne eTwo) xs 	= (evalExpr eOne xs) >= (evalExpr eTwo xs)
-evalCmp (Cmp NotEqual eOne eTwo) xs 		= (evalExpr eOne xs) /= (evalExpr eTwo xs)
+evalCmp (Cmp GreaterEqual eOne eTwo) xs = (evalExpr eOne xs) >= (evalExpr eTwo xs)
+evalCmp (Cmp NotEqual eOne eTwo) xs 	= (evalExpr eOne xs) /= (evalExpr eTwo xs)
 
