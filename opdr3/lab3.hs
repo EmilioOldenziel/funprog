07ffd7efcf3892e28fe3a6ad94d5d6801de48249
S1959492 <s1959492@fwn-bborg-edu-74-47.fmnsedu.rug.nl>
2014-10-24 16:16:26 +0200

    evalCmp klaar

--- opdr3/lab3.hs
+++ opdr3/lab3.hs
@@ -69,11 +69,10 @@ checkOperator ">=" = GreaterEqual
 checkOperator "#" = NotEqual
 
 evalCmp :: Comparison -> Valuation -> Bool
-evalCmp deCmpInput xs 
- |(Cmp LessThan eOne eTwo) xs 		= (evalExpr eOne xs) < 	(evalExpr eTwo xs)
- |(Cmp LessEqual eOne eTwo) xs 		= (evalExpr eOne xs) <= (evalExpr eTwo xs)
- |(Cmp Equal eOne eTwo) xs 			= (evalExpr eOne xs) == (evalExpr eTwo xs)
- |(Cmp Greater eOne eTwo) xs 		= (evalExpr eOne xs) > 	(evalExpr eTwo xs)
- |(Cmp GreaterEqual eOne eTwo) xs 	= (evalExpr eOne xs) >= (evalExpr eTwo xs)
- |(Cmp NotEqual eOne eTwo) xs 		= (evalExpr eOne xs) /= (evalExpr eTwo xs)
+evalCmp (Cmp LessThan eOne eTwo) xs 		= (evalExpr eOne xs) < 	(evalExpr eTwo xs)
+evalCmp (Cmp LessEqual eOne eTwo) xs 		= (evalExpr eOne xs) <= (evalExpr eTwo xs)
+evalCmp (Cmp Equal eOne eTwo) xs 			= (evalExpr eOne xs) == (evalExpr eTwo xs)
+evalCmp (Cmp Greater eOne eTwo) xs 		= (evalExpr eOne xs) > 	(evalExpr eTwo xs)
+evalCmp (Cmp GreaterEqual eOne eTwo) xs 	= (evalExpr eOne xs) >= (evalExpr eTwo xs)
+evalCmp (Cmp NotEqual eOne eTwo) xs 		= (evalExpr eOne xs) /= (evalExpr eTwo xs)
 
